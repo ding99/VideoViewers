9c76d440f10e7ad88253d02c5b6fe5646b558c15
Wei Ding <ding_web@yahoo.co.jp>
2020-01-02 16:48:41 -0500

    main page UI update

--- TitleViewer/MainPage.xaml.cs
+++ TitleViewer/MainPage.xaml.cs
@@ -22,17 +22,55 @@ namespace TitleViewer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private List<Content> contents;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            contents = new List<Content> {
+                new Content("Title1", "Bullet1", "Description1", "A1", 10),
+                new Content("Zitle2", "Bullet2", "Description2", "A2", 20),
+                new Content("Title3", "Bullet3", "Description3", "A3", 30),
+                new Content("Title4", "Bullet4", "Description4", "A4", 40),
+                new Content("Title5", "Bullet5", "Description5", "A5", 50)
+            };
+
+            SetSource();
         }
+        private void SetSource()
+        {
+            Titles.ItemsSource = contents = contents.OrderBy(p => p.title).ToList();
+            StatusMain.Text = contents.Count().ToString();
+        }
+
         private void Key_Down(object sender, KeyRoutedEventArgs e)
         {
             if (e.Key == Windows.System.VirtualKey.Enter)
             {
-                //TODO
+                GetItems(Input.Text);
             }
         }
 
+        private void Title_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //TODO
+        }
+
+        public void GetItems(string endpoint)
+        {
+            contents = new List<Content> {
+                new Content("New Title1", "Bullet1", "Description1", "A1", 10),
+                new Content("New Title2", "Bullet2", "Description2", "A2", 20),
+                new Content("New Title3", "Bullet3", "Description3", "A3", 30),
+                new Content("New Title4", "Bullet4", "Description4", "A4", 40),
+                new Content("New Title5", "Bullet5", "Description5", "A5", 50),
+                new Content("New Title6", "Bullet6", "Description6", "A5", 60),
+                new Content("New Title7", "Bullet7", "Description7", "A5", 70)
+            };
+
+            SetSource();
+        }
     }
 }
