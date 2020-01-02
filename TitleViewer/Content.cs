d8e53ed93bb9abd4029250ed706ea5dc65674003
Wei Ding <ding_web@yahoo.co.jp>
2020-01-02 16:15:48 -0500

    video data definition

---
+++ TitleViewer/Content.cs
@@ -0,0 +1,20 @@
+﻿namespace TitleViewer
+{
+    public class Content
+    {
+        public string title { get; set; }
+        public string bulletText { get; set; }
+        public string description { get; set; }
+        public string id { get; set; }
+        public int runningTime { get; set; }
+
+        public Content(string title, string bulletText, string description, string id, int runningTime)
+        {
+            this.title = title;
+            this.bulletText = bulletText;
+            this.description = description;
+            this.id = id;
+            this.runningTime = runningTime;
+        }
+    }
+}
